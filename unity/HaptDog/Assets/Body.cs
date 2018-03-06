d69670d2a798e462015cf849481cd99953e48cf0
Hideyuki Takei <takehide22@gmail.com>
2018-03-06 23:26:45 +0900

    Add HaptDog unity project

---
+++ unity/HaptDog/Assets/Body.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+		float x = Input.GetAxis("Vertical");
+
+		Vector3 pos = transform.position;
+		pos.x += x * 0.5f;
+		transform.position = pos;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ unity/HaptDog/Assets/LegLeft.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegLeft : MonoBehaviour {
+	HingeJoint joint;
+	float currentPosition;
+	float targetPosition;
+	float beforeTime;
+
+	// Use this for initialization
+	void Start () {
+		joint = gameObject.GetComponent<HingeJoint>();
+		currentPosition = 0.0f;
+		targetPosition = 0.0f;
+		beforeTime = Time.time;
+	}
+
+	private void FixedUpdate()
+	{
+		float x = Input.GetAxis("Vertical");
+
+		targetPosition += x * 3.0f;
+
+		if (targetPosition > 90.0f) {
+			targetPosition = 90.0f;
+		} else if (targetPosition < -90.0f) {
+			targetPosition = -90.0f;
+		}
+
+		float diffPosition = (Time.time - beforeTime) * 100.0f;
+		beforeTime = Time.time;
+
+		if (Mathf.Abs (targetPosition - currentPosition) < diffPosition) {
+			currentPosition = targetPosition;
+		} else {
+			if (targetPosition > currentPosition) {
+				currentPosition += diffPosition;
+			} else {
+				currentPosition -= diffPosition;
+			}
+		}
+
+		JointSpring hingeSpring = joint.spring;
+		hingeSpring.targetPosition = currentPosition;
+		joint.spring = hingeSpring;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
